08ff7b42a679f7eb200c00e7998fdcff88ddcb14
draou <meryem.draou@etu.univ-amu.fr>
2022-10-31 16:11:53 +0100

    Ajout de fonctionnalités

--- src/main/java/graphic/GraphicEntity.java
+++ src/main/java/graphic/GraphicEntity.java
@@ -44,6 +44,15 @@ public class GraphicEntity extends JPanel {
     g.drawImage(image, (int) coordinates.getX(), (int) coordinates.getY(), null);
   }
 
+  public void setOpacity(float opacity) {
+    BufferedImage resizedImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
+    Graphics2D graphics = resizedImage.createGraphics();
+    graphics.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, opacity));
+    graphics.drawImage(image, 0, 0, image.getWidth(), image.getHeight(), null);
+    graphics.dispose();
+
+  }
+
   public void setCoordinates(Point2D newCoordinates) {
     coordinates = newCoordinates;
   }
@@ -53,5 +62,21 @@ public class GraphicEntity extends JPanel {
   }
 
   public void rotate(double angle) {
+    double radian = Math.toRadians(angle), sin = Math.abs(Math.sin(radian)), cos = Math.abs(Math.cos(radian));
+
+    int newWidth = (int) Math.floor((double) image.getWidth() * cos + (double) image.getHeight() * sin);
+    int newHeight = (int) Math.floor((double) image.getHeight() * cos + (double) image.getWidth() * sin);
+
+    BufferedImage rotatedImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_ARGB);
+
+    Graphics2D graphics = rotatedImage.createGraphics();
+
+    graphics.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+
+    graphics.translate((newWidth - image.getWidth()) / 2, (newHeight - image.getHeight()) / 2);
+    // rotation around the center point
+    graphics.rotate(radian, (double) (image.getWidth() / 2), (double) (image.getHeight() / 2));
+    graphics.drawImage(image, 0, 0, null);
+    graphics.dispose();
   }
 }
