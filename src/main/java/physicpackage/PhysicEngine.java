48c2d269566a8c8ae1af6b17619c6b73ad6b364e
MarinasAlonso <marinas.alonso@outlook.com>
2022-10-31 17:05:13 +0100

    push-funny-de-malade

--- src/main/java/physicpackage/PhysicEngine.java
+++ src/main/java/physicpackage/PhysicEngine.java
@@ -1,8 +1,22 @@
 package physicpackage;
 
-//import java.util.ArrayList;
-//import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PhysicEngine {
-    // private List<PhysicalEntity> physicalEntities = new ArrayList<>();
+    private List<PhysicalEntity> physicalEntities = new ArrayList<>();
+
+    public void addPhysicalEntity(PhysicalEntity physicalEntity) {
+        physicalEntities.add(physicalEntity);
+    }
+
+    public PhysicalEntity getPhysicalEntity(int index) {
+        return physicalEntities.get(index);
+    }
+
+    public void update() {
+        for (PhysicalEntity physicalEntity : physicalEntities) {
+            physicalEntity.update();
+        }
+    }
 }
--- src/main/java/physicpackage/PhysicalEntity.java
+++ src/main/java/physicpackage/PhysicalEntity.java
@@ -1,10 +1,16 @@
 package physicpackage;
 
 public class PhysicalEntity {
+    private int id;
     private Transition transition;
 
-    public PhysicalEntity(Transition transition) {
+    public PhysicalEntity(Transition transition, int id) {
         this.transition = transition;
+        this.id = id;
+    }
+
+    public int getID() {
+        return this.id;
     }
 
     public Transition getTransition() {
