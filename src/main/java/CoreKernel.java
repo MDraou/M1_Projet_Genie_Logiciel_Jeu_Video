212f45edf034c802a39a3a06fd5d09faaf62e20b
ARTAUD Cyril <cyril.artaud@etu.univ-amu.fr>
2022-10-31 17:37:11 +0100

    creation des fonctions sous la dictature d'Alonso

--- src/main/java/CoreKernel.java
+++ src/main/java/CoreKernel.java
@@ -1,2 +1,30 @@
+import physicpackage.PhysicEngine;
+
 public class CoreKernel {
+    private PhysicEngine physicEngine;
+    //private GraphicEngine graphicEngine;
+
+
+    public CoreKernel(PhysicEngine physicEngine) {
+        this.physicEngine = physicEngine;
+        //this.graphicEngine = graphicEngine;
+    }
+
+    public void update() {
+
+    }
+
+    public void start() {
+
+    }
+
+    public int createGraphicEntity() { //Image image
+
+        return 0;
+    }
+
+    public int createPhysicEntity() {
+
+        return 0;
+    }
 }
