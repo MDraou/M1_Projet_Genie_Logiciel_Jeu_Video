6d8fd0cd1097b00c12c767fb7cb62717aec362c1
Adrien Vidal <adrien.vidal.1@etu.univ-amu.fr>
2022-12-04 22:10:53 +0100

    Avancement de Pong

---
+++ src/main/java/gameplay/Visitor.java
@@ -0,0 +1,39 @@
+package gameplay;
+
+import engine.kernel.EntityBuilder;
+
+public class Visitor implements ElementVisitor {
+
+    private final EntityBuilder builder;
+
+    public Visitor(EntityBuilder builder) {
+        this.builder = builder;
+    }
+
+    @Override
+    public void visit(Wall wall) {
+        builder.setEntity(wall.getEntity());
+        builder.buildSprite(wall.getImagePath(), wall.getWidth(), wall.getHeight());
+        builder.buildMovementChecker(wall.getWidth(), wall.getHeight(), false);
+    }
+
+    @Override
+    public void visit(Ball ball) {
+        builder.setEntity(ball.getEntity());
+        builder.buildSprite(ball.getImagePath(), ball.getWidth(), ball.getHeight());
+        builder.buildMovementChecker(ball.getWidth(), ball.getHeight(), true);
+    }
+
+    @Override
+    public void visit(Paddle paddle) {
+        builder.setEntity(paddle.getEntity());
+        builder.buildSprite(paddle.getImagePath(), paddle.getWidth(), paddle.getHeight());
+        builder.buildMovementChecker(paddle.getWidth(), paddle.getHeight(), false);
+    }
+
+    @Override
+    public void visit(Score score) {
+        builder.setEntity(score.getEntity());
+        builder.buildSprite(score.getImagePath(), score.getWidth(), score.getHeight());
+    }
+}
